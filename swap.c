a21c349d4373f1d65c30b1c9e73daceea3bd73a1
RakameNouya <erlanggarahmansyah25@gmail.com>
2026-02-22 12:13:21 +0700

    swap.c filled

---
+++ swap.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a = 5, b = 10;
+    printf("Before swap: a = %d, b = %d\n", a, b);
+
+    // Swap using a temporary variable    
+    printf("After swap: a = %d, b = %d\n", a, b);
+    return 0;
+}
