b17af8fcc898f96a8649e573c4047a65d0caadee
RakameNouya <erlanggarahmansyah25@gmail.com>
2026-02-22 17:35:50 +0700

    newest swap.c

--- swap.c
+++ swap.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 int main() {
     int a = 5, b = 10;
     printf("Before swap: a = %d, b = %d\n", a, b);
 
     // Swap using a temporary variable  
-    int temp = a;
-    a = b;
-    b = temp;
+    swap(&a, &b);
     
     printf("After swap: a = %d, b = %d\n", a, b);
     return 0;
